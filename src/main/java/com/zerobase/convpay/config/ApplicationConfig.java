b73af94fe7619ffb54749a95ebcd18d69e2a15fb
DYS <kcood92@gmail.com>
2022-08-31 13:47:34 +0900

    [refactor] 001-CHANGE-TO-SPRING

--- src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
+++ src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
@@ -1,28 +1,45 @@
 package com.zerobase.convpay.config;
 
 import com.zerobase.convpay.service.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
+@Configuration
 public class ApplicationConfig {
 
-    public ConveniencePayService conveniencePayServiceDiscountPayMethod(){
+//    public ConveniencePayService conveniencePayService(){
+//        return new ConveniencePayService(
+//                new HashSet<>(
+//                        Arrays.asList(new MoneyAdaptor(), new CardAdaptor())
+//                ),
+//                new DiscountByPayMethod()
+//        );
+//    }
+
+    @Bean
+    public ConveniencePayService conveniencePayService() {
         return new ConveniencePayService(
-                new HashSet<>(
-                        Arrays.asList(new MoneyAdaptor(), new CardAdaptor())
-                ),
-                new DiscountByPayMethod()
+                new HashSet<>(Arrays.asList(moneyAdaptor(), cardAdaptor())),
+                discountByConvenience()
         );
     }
 
-    public ConveniencePayService conveniencePayServiceDiscountConvenience(){
-        return new ConveniencePayService(
-                new HashSet<>(
-                        Arrays.asList(new MoneyAdaptor(), new CardAdaptor())
-                ),
-                new DiscountByConvenience()
-        );
+    @Bean
+    public CardAdaptor cardAdaptor() {
+        return new CardAdaptor();
+    }
+
+    @Bean
+    public MoneyAdaptor moneyAdaptor() {
+        return new MoneyAdaptor();
+    }
+
+    @Bean
+    public DiscountByConvenience discountByConvenience() {
+        return new DiscountByConvenience();
     }
 
 }
